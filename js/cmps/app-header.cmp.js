bc2ec75f8770990f8ecf0f1eea35bae1c0f1289a
Yarden Mathieson <yardenm2710@gmail.com>
2021-11-10 11:25:01 +0200

    balbal

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,11 +1,12 @@
 export default {
-    template: `
+  template: `
         <header class="app-header">
             <div class="logo">
              <a  class="logo-link" href="/"><h3>Appsus</h3></a>
              </div>
              <nav class="main-nav">
             <router-link @click.native="scrollToTop"  to="/">Home</router-link>
+            <h1>ahlan</h1>
 
              <router-link @click.native="scrollToTop"  to="/email">emails</router-link>
              <router-link @click.native="scrollToTop"  to="/keep">keep</router-link>
@@ -14,9 +15,9 @@ export default {
             </nav>
         </header>
     `,
-    methods: {
-        scrollToTop() {
-            window.scrollTo(0, 0);
-        }
-    }
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    },
+  },
 }
