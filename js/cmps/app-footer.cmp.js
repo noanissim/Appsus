7262d301943e0c7282160f7965cfc0daee48694b
Noa Nissim <87809765+noanissim@users.noreply.github.com>
2021-11-13 21:37:10 +0200

    noa 11 commit

--- js/cmps/app-footer.cmp.js
+++ js/cmps/app-footer.cmp.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <footer class="app-footer">
-            <p>&copy; Coffeerights 2021</p>
+            <p>&copy; Copyrights 2021</p>
         </footer>
     `,
 }
