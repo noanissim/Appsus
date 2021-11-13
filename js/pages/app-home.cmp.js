048b97436a6fa7ef3e79bf234b322c410b893ae4
Yarden Mathieson <yardenm2710@gmail.com>
2021-11-13 21:31:16 +0200

    yarden final commit

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -6,7 +6,7 @@ export default {
         <div class="main-home-container">
         
             <div class=home-details-container>
-                <h1>Easy and secure access to all of your content</h1>
+                <h1>Every moment is a fresh beginning</h1>
                 <p>Store, share, and collaborate on files and folders from any mobile device, tablet, or computer</p>
                 <div class="action-btn-home">
                         <router-link @click.native="scrollToTop"  to="/email">Emails</router-link>
@@ -14,7 +14,7 @@ export default {
                         <router-link @click.native="scrollToTop"  to="/book">Books</router-link>
                 </div>
             </div>
-                <img src="./img/home.png"/>
+                <img src="./img/home1.png"/>
         </div>
 
         </section>
